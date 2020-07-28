54f87fac96f9beffb776c11cff4652dd6a2846a1
helensnee <helensnee@lifesites.com.au>
2020-07-28 21:11:03 +1000

    Adding a Menu to Victorian Flora

--- site/views/helloworld/view.html.php
+++ site/views/helloworld/view.html.php
@@ -6,13 +6,30 @@
  * @copyright   Copyright (C) 2005 - 2018 Open Source Matters, Inc. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
+
 // No direct access to this file
 defined('_JEXEC') or die('Restricted access');
+
 /**
- * Hello World Component Controller
+ * HTML View class for the HelloWorld Component
  *
  * @since  0.0.1
  */
-class HelloWorldController extends JControllerLegacy
+class HelloWorldViewHelloWorld extends JViewLegacy
 {
+	/**
+	 * Display the Hello World view
+	 *
+	 * @param   string  $tpl  The name of the template file to parse; automatically searches through the template paths.
+	 *
+	 * @return  void
+	 */
+	function display($tpl = null)
+	{
+		// Assign data to the view
+		$this->msg = 'Hello World';
+
+		// Display the view
+		parent::display($tpl);
+	}
 }
